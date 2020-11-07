a96135aebc64c3deaf3838043de6ced5bebac368
MFcortez <mep.filho2012@gmail.com>
2020-11-07 11:08:02 -0300

    Up calculadora

---
+++ AC2/POO_14_09/POO_14_09_20/Calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace POO_14_09_20
+{
+    public class Calculadora
+    {
+        public static double cambio = 5.24;
+
+        public static double Recalc(double val)
+        {
+            return val * cambio + ((val * cambio) * 0.6);
+        }
+    }
+}
---
+++ AC2/POO_14_09/POO_14_09_20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_14_09_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco;
+
+            Console.Write("Informe o valor do produto: ");
+            preco = Double.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
