76f9f72b177b4f985a0f47b4d011b7c4b7e77b5a
MFcortez <mep.filho2012@gmail.com>
2020-11-18 16:44:48 -0300

    Up do exercício do dia 26_10
    Exercício 2

---
+++ AC2/POO_26_10/POO_26_10/Funcionario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+public class Funcionario
+{
+	//Variáveis
+	public string Id;
+	string Nome;
+	double Salario;
+
+	#region Construtor de ID da conta
+	private string ConstructID()
+	{
+		string _generated;
+		Random num = new Random();
+
+		_generated = num.Next(10).ToString();
+		for (int i = 0; i < 2; i++)
+		{
+			_generated = _generated + num.Next(10).ToString();
+		}
+
+		return _generated;
+	}
+
+	#endregion
+
+	//Construtor
+	public Funcionario(string nome, double salario)
+	{
+		Id = ConstructID();
+		Nome = nome;
+		Salario = salario;
+	}
+
+	//Aumentar Salário
+	public void aumentarSalario(double porcetagem)
+	{
+		porcetagem /= 100;
+		porcetagem += 1;
+		Salario *= porcetagem;
+	}
+
+	//ToString
+	public override string ToString()
+	{
+		return "Dados do Funcionário: \nID:" + Id
+			+ "\nNOME: " + Nome
+			+ "\nSALÁRIO: R$" + Salario.ToString("F2", CultureInfo.InvariantCulture);
+	}
+}
---
+++ AC2/POO_26_10/POO_26_10/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace POO_26_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando número de funcionários e lista
+            Console.Write("Quantos funcionários serão registrados: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            List<Funcionario> funcionarios = new List<Funcionario>(n);
+
+            //Cadastrando os funcionários com o construtor
+            for(int i = 0; i<n; i++)
+            {
+                Console.Write("Insira o nome do funcionário: ");
+                string nome = Console.ReadLine();
+                Console.Write("Insira o salário do funcionário: ");
+                double salario = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                funcionarios.Add(new Funcionario(nome, salario));
+                Console.WriteLine("FUNCIONÁRIO CADASTRADO \n");
+            }
+
+            //Exibindo os Funcionários
+            foreach(Funcionario obj in funcionarios)
+            {
+                Console.WriteLine("\n" + obj.ToString() + "\n");
+            }
+
+            //Pergunta ID do funcionário que receberá aumento e valida
+            bool valido = false;
+            while (valido == false)
+            {
+                Console.Write("ID do funcionário que terá aumento: ");
+                int x = Int32.Parse(Console.ReadLine());
+                Console.Write("Insira o valor em porcentagem do aumenta: %");
+                double aumento = Double.Parse(Console.ReadLine());
+
+
+                foreach (Funcionario obj in funcionarios)
+                {
+                    //Caso o ID seja condizente com algum funcionário, while acaba. Se não, while repete
+                    if (obj.Id == x.ToString())
+                    {
+                        obj.aumentarSalario(aumento);                      
+                        valido = true;
+                    }
+                }
+            }
+
+            //Exibe Funcionários
+            foreach (Funcionario obj in funcionarios)
+            {
+                Console.WriteLine("\n" + obj.ToString() + "\n");
+            }
+
+        }
+    }
+}
